967f92ac5d5bc1c2a91aad74f7c37d86f38583b9
UnDer-7 <mateus7532@gmail.com>
2024-11-24 21:47:10 -0300

    processing services

--- src/main/java/org/study/processamentoplanilhas/controller/ProcessSpreadsheetController.java
+++ src/main/java/org/study/processamentoplanilhas/controller/ProcessSpreadsheetController.java
@@ -1,12 +1,27 @@
 package org.study.processamentoplanilhas.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.study.processamentoplanilhas.ProcessamentoPlanilhasApplication;
+import org.study.processamentoplanilhas.entity.CarroSpreadsheet;
+import org.study.processamentoplanilhas.service.ProcessSpreadsheetService;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @Slf4j
 @Validated
@@ -14,9 +29,26 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/api/v1/process")
 public class ProcessSpreadsheetController {
 
-    @PostMapping
-    public void process(@RequestParam("spreadsheet") MultipartFile file) {
-        log.info("Receiving new spreadsheets. File name: {} | File ContentType: {}", file.getOriginalFilename(), file.getContentType());
+    private final ProcessSpreadsheetService processSpreadsheetService;
+
+    public ProcessSpreadsheetController(ProcessSpreadsheetService processSpreadsheetService) {
+        this.processSpreadsheetService = processSpreadsheetService;
     }
 
+
+    @PostMapping
+    public void process(@RequestParam("spreadsheet") MultipartFile file) throws IOException {
+        log.info("Receiving new spreadsheets. File name: {} | File ContentType: {}", file.getOriginalFilename(), file.getContentType());
+
+        String filename = file.getOriginalFilename();
+
+        List<CarroSpreadsheet> dtos;
+
+        if (!filename.endsWith(".xlsx") && !filename.endsWith(".xls")) {
+            throw new UnsupportedOperationException("Spreadsheet file extension is not supported");
+        }
+
+        dtos = processSpreadsheetService.processExcelFile(file);
+        log.info(dtos.toString());
+    }
 }
